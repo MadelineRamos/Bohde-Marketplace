a362f5898edd2119bf982e8330ea6d9900ff7ec0
Joshua Bohde <bohdecoded@gmail.com>
2022-11-25 13:00:17 -0500

    fix: render single post page correctly

--- models/Post.js
+++ models/Post.js
@@ -5,53 +5,57 @@ const User = require('./User');
 class Post extends Model { }
 
 Post.init(
-    {
-        post_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        //from user
-        seller_id: {
-            type: DataTypes.UUID,
-            references: {
-                model: "user",
-                key: "id",
-            },
-            allowNull: false,
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        price: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        // from category
-        category_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "category",
-                key: "id",
-            },
-            allowNull: false,
-        },
-        image_url: {
-            type: DataTypes.STRING,
-        },
-        description: {
-            type: DataTypes.STRING,
-        }
+  {
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'post',
+    //from user
+    seller_id: {
+      type: DataTypes.UUID,
+      references: {
+        model: "user",
+        key: "id",
+      },
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    // from category
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+      allowNull: false,
+    },
+    image_url: {
+      type: DataTypes.STRING,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
     }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
 );
 
 module.exports = Post;
